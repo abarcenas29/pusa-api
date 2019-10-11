69fec044dbc26a407b435f9329d9880931ded4c5
Aldrich Allen Barcenas <aldrich.barcenas@gmail.com>
2019-10-11 17:01:03 +0800

    Add check-in func

--- methods/times.js
+++ methods/times.js
@@ -2,22 +2,22 @@ const Times = require('./../models/Times')
 
 module.exports = {
   createTimes: args => {
-    return Stores.create(args)
+    return Times.create(args)
   },
   findTimes: args => {
     if (args.limit) args.limit = parseInt(args.limit)
     if (args.offset) args.offset = parseInt(args.offset)
 
-    return Stores.findAndCountAll(args)
+    return Times.findAndCountAll(args)
   },
   updateTimes: args => {
     const options = args.options || {}
     delete args.options
-    return Stores
+    return Times
       .update(args, options)
   },
   deleteTimes: args => {
-    return Stores.destroy({
+    return Times.destroy({
       where: args
     })
   }
--- routes/times.js
+++ routes/times.js
@@ -1,23 +1,39 @@
 const express = require('express')
 const TimesMethods = require('./../methods/times')
 
+const helpers = require('./../helpers/response')
+
 const TimeRouters = express.Router()
 
 TimeRouters
   .route('/time')
-  .get((req, res) => {
-    TimesMethods
-      .findTimes(req.query)
-      .then(output => {
-        res.json(output)
-      })
-  })
   .post((req, res) => {
-    TimesMethods
-      .createTimes(req.body)
-      .then(output => {
-        res.json(output)
-      })
+    const { action, ...rest } = req.body
+    switch (action) {
+      case 'create':
+        return TimesMethods
+          .createTimes(req.body)
+          .then(o => {
+            res.json(
+              helpers.wrapper(o) || 
+              helpers.wrapper({ error: 'Error retrieving list'})
+            )
+          })
+        break
+      case 'query':
+        return TimesMethods
+          .findTimes(req.body)
+          .then(o => {
+            res.json(
+              helpers.wrapper(o) || 
+              helpers.wrapper({ error: 'Error retrieving list'})
+            )
+          })
+        break
+      default:
+        return helpers.wrapper({error: 'Invalid query'})
+        break
+    }
   })
   .put((req, res) => {
     TimesMethods
